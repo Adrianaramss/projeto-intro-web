46b99be92eabb097c4f36ee70c16a69564f2c4bd
Adriana <adrianascosta9@gmail.com>
2022-09-23 19:04:33 -0300

    semana3

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+// -----------------------semana1-------------------------------------------
 
+// 3. Faça um cálculo de média, entre os valores numéricos respectivos de cada item.
+//  Imprima o valor da média utilizando um console.log. Guarde este valor em uma const;
+
+// const guardamédia =  ((ator1.idade + ator2.idade+ator3.idade) / 3)
+// console.log("O valor da média é ", (guardamédia))
+
+// 4. Com um console.log, imprima o resultado de uma operação lógica que checa se todos os 
+// valores de variáveis booleanas criadas até aqui são verdadeiras.
+
+//  console.log ("Todos atuaram como super-héroi?" + atuoucomosuperheroi1 && atuoucomosuperheroi2 && atuoucomosuperheroi3)
+
+
+// 5.Crie pelo menos mais uma característica para o item que você criou. Esta característica deve ser um array.
+//  Mantenha o tipo de dado do array criado sempre o mesmo, isto é, se é um array de strings, só deve receber strings
+
+//  const arrayfilmesqueparticipou1 = ["Thor","Vingadores","Resgate"]  /// ATOR CHRIS HEMSWORTH
+//  const arrayfilmesqueparticipou2 = ["Top Gun", "Missão Impossível", "A Múmia"]  //ATOR TOM CRUISE
+//  const arrayfilmesqueparticipou3 = ["Vingadores","Homem De Ferro","Dolittle"]   //ATOR ROBERT DOWNEY 
+
+
+// 6. Exiba um relatório utilizando console.log(), mostrando todos os dados de cada um dos itens criados até aqui
+//   1. O log deve exibir o dado de nome, título ou afim sempre em LETRAS MAIÚSCULAS, como no exemplo abaixo.
+
+//  console.log( nome1.toUpperCase())
+//  console.log ("idade:" +  idade1 )
+//  console.log ("Atuou como super-héroi?" + atuoucomosuperheroi1)
+//  console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou1)
+
+
+//  console.log( nome2.toUpperCase())
+//  console.log ("idade:" +  idade2 )
+//  console.log ("Atuou como super-héroi?" + atuoucomosuperheroi2)
+//  console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou2)
+
+//  console.log( nome3.toUpperCase())
+//  console.log ("idade:" +  idade3 )
+//  console.log ("Atuou como super-héroi?" + atuoucomosuperheroi3)
+//  console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou3)
+
+//-----------------------semana2-------------------------------------------
+//============ Ator 1 =====================================
 
 ator1 = {
    nome : "Chris Hemsworth",
@@ -6,9 +48,9 @@ ator1 = {
    atuoucomosuperheroi : true,
    arrayfilmesparticipou:["Thor","Vingadores","Resgate"]
 }
-console.log (`Ator: \"${ator1.nome}\n idade: ${ator1.idade}\n atuou como Super-heroi? ${ator1.atuoucomosuperheroi}\n filmes que atuou: ${ator1.arrayfilmesparticipou} `)
 
 
+//============ Ator 2 =====================================
 ator2 = {
     nome : "Tom Cruise",
     idade : 60,
@@ -16,7 +58,8 @@ ator2 = {
     arrayfilmesparticipou: ["Top Gun", "Missão Impossível", "A Múmia"]
 
 }
-console.log (`Ator: \"${ator2.nome}\n idade: ${ator2.idade}\n atuou como Super-heroi? ${ator2.atuoucomosuperheroi}\n filmes que atuou: ${ator2.arrayfilmesparticipou} `)
+
+//============ Ator 3 =====================================
 
 ator3 = {
     nome : "Robert Downey",
@@ -24,23 +67,23 @@ ator3 = {
     atuoucomosuperheroi : true,
     arrayfilmesparticipou:["Vingadores","Homem De Ferro","Dolittle"]
 }
-console.log (`Ator: \"${ator3.nome}\n idade: ${ator3.idade}\n atuou como Super-heroi? ${ator3.atuoucomosuperheroi}\n filmes que atuou: ${ator3.arrayfilmesparticipou} `)
 
+///==========================================================
 //Crie um array para guardar os objetos. Este array deve estar vazio, por enquanto;
 const atores = []  /// array para guardar objetos
 
 //adiconar objetos ao array 
 //atores.push(ator1,ator2,ator3)
-//console.log(atores)
+//console.log(atores) 
 
-///verificação 
+//============ verificação Ator 1/2/3/ =======================
 
-if (ator1.atuoucomosuperheroi === true) {
+if (ator1.atuoucomosuperheroi === true) { /// verdadeira 
     atores.push(ator1)
 } else {
     alert (ator1.nome  + " não atuou como super-heroi")
 }
-if (ator2.atuoucomosuperheroi === true) {
+if (ator2.atuoucomosuperheroi === true) { ///falso
      //atores.push(ator2)
 
 } else {
@@ -53,47 +96,88 @@ if (ator3.atuoucomosuperheroi === true) {
     alert (ator3.nome + " não atuou como super-heroi")
 }
 
+//-----------------------semana3-------------------------------------------
+//1 e 2
 
+//============ Ator 1 =====================================
 
+for(let i in  ator1.arrayfilmesparticipou) {
+    stringarrayfilmesparticipou = ator1.arrayfilmesparticipou.join()
+   // console.log(stringarrayfilmesparticipou);
 
-//3. Faça um cálculo de média, entre os valores numéricos respectivos de cada item.
-// Imprima o valor da média utilizando um console.log. Guarde este valor em uma const;
+  }
+   console.log(` Ator: ${ator1.nome} \n idade:${ator1.idade} \n atuou como Super-heroi? ${ator1.atuoucomosuperheroi} \n filmes que atuou:"${stringarrayfilmesparticipou}"`);
 
-const guardamédia =  ((ator1.idade + ator2.idade+ator3.idade) / 3)
-console.log("O valor da média é ", (guardamédia))
+//============ Ator 2 =====================================
 
+   for(let i in  ator2.arrayfilmesparticipou) {
+    stringarrayfilmesparticipou = ator2.arrayfilmesparticipou.join()
+    //console.log(stringarrayfilmesparticipou);
 
+  }
+   console.log(` Ator: ${ator2.nome} \n idade:${ator2.idade} \n atuou como Super-heroi? ${ator1.atuoucomosuperheroi} \n filmes que atuou:"${stringarrayfilmesparticipou}"`);
 
-//4. Com um console.log, imprima o resultado de uma operação lógica que checa se todos os 
-//valores de variáveis booleanas criadas até aqui são verdadeiras.
+//============ Ator 3 =====================================
 
- ///console.log ("Todos atuaram como super-héroi?" + atuoucomosuperheroi1 && atuoucomosuperheroi2 && atuoucomosuperheroi3)
+   for(let i in  ator3.arrayfilmesparticipou) {
+    stringarrayfilmesparticipou = ator3.arrayfilmesparticipou.join()
+    //console.log(stringarrayfilmesparticipou);
+  }
+   console.log(` Ator: ${ator3.nome} \n idade:${ator3.idade} \n atuou como Super-heroi? ${ator1.atuoucomosuperheroi} \n filmes que atuou:"${stringarrayfilmesparticipou}"`);
 
+// 3. Crie uma função que receba como parâmetro um objeto, e devolva a
+// string do relatório com os dados do objeto.
 
-//5.Crie pelo menos mais uma característica para o item que você criou. Esta característica deve ser um array.
- //Mantenha o tipo de dado do array criado sempre o mesmo, isto é, se é um array de strings, só deve receber strings
+ const funcao = (ator1) => {
+  for(let i in ator1) {
+    console.log(`${ator1.nome} \n ${ator1.idade} \n ${ator1.atuoucomosuperheroi} \n ${ator1.arrayfilmesparticipou}`)
+  }
+   }
+   funcao(ator1)
 
-// const arrayfilmesqueparticipou1 = ["Thor","Vingadores","Resgate"]  /// ATOR CHRIS HEMSWORTH
-// const arrayfilmesqueparticipou2 = ["Top Gun", "Missão Impossível", "A Múmia"]  //ATOR TOM CRUISE
-// const arrayfilmesqueparticipou3 = ["Vingadores","Homem De Ferro","Dolittle"]   //ATOR ROBERT DOWNEY 
+// 4. Crie uma função que recebe um array de objetos e uma string.
+// Esta função deve retornar um objeto, e o objeto retornado deve 
+// possuir apenas os itens que tenham o nome/título igual à string passada
+// como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
 
+let todosatores = [
+{
+    nome : "Chris Hemsworth",
+    idade : 39,
+    atuoucomosuperheroi : true,
+    arrayfilmesparticipou:["Thor","Vingadores","Resgate"]
+},
+{
 
-//6. Exiba um relatório utilizando console.log(), mostrando todos os dados de cada um dos itens criados até aqui
- // 1. O log deve exibir o dado de nome, título ou afim sempre em LETRAS MAIÚSCULAS, como no exemplo abaixo.
+    nome : "Tom Cruise",
+    idade : 60,
+    atuoucomosuperheroi : false,
+    arrayfilmesparticipou: ["Top Gun", "Missão Impossível", "A Múmia"]
 
-// console.log( nome1.toUpperCase())
-// console.log ("idade:" +  idade1 )
-// console.log ("Atuou como super-héroi?" + atuoucomosuperheroi1)
-// console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou1)
+},
+{
+    nome : "Robert Downey",
+    idade : 57,
+    atuoucomosuperheroi : true,
+    arrayfilmesparticipou:["Vingadores","Homem De Ferro","Dolittle"]
 
+}
 
-// console.log( nome2.toUpperCase())
-// console.log ("idade:" +  idade2 )
-// console.log ("Atuou como super-héroi?" + atuoucomosuperheroi2)
-// console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou2)
+]
 
-// console.log( nome3.toUpperCase())
-// console.log ("idade:" +  idade3 )
-// console.log ("Atuou como super-héroi?" + atuoucomosuperheroi3)
-// console.log ("Filmes que Atuou?" + arrayfilmesqueparticipou3)
+const funcao2 =  (todosatores,nome) => {
+let nomesatores = 0   
+for(let i of todosatores) {
+    if(i.nome.toLowerCase() === nome.toLowerCase()){
+     console.log(i) 
+     nomesatores++ 
+}     
+} 
+
+ if(nomesatores === 0) {
+    alert("nenhum item encontrado!")
+}
+}
+let verificarnomeautor = prompt("Digite o nome de um ator")
+funcao2(todosatores,verificarnomeautor)
 
